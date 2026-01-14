2f2ac4a5ff3d360f30d0abbd2ae089abaa953071
Work <yakushevso@ya.ru>
2026-01-14 15:33:48 +0300

    Initial commit

---
+++ src/main/java/com/yakushevso/vacation_calc_neoflex/VacationCalcNeoflexApplication.java
@@ -0,0 +1,12 @@
+package com.yakushevso.vacation_calc_neoflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationCalcNeoflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationCalcNeoflexApplication.class, args);
+	}
+}
---
+++ src/main/java/com/yakushevso/vacation_calc_neoflex/model/VacationCalcNeoflexResponse.java
@@ -0,0 +1,10 @@
+package com.yakushevso.vacation_calc_neoflex.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VacationCalcNeoflexResponse {
+    private double amount;
+}
